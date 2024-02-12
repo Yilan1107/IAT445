822ab7759e4d379b35beec2e112cf35ad65551e3
Yilan1107 <159740644+Yilan1107@users.noreply.github.com>
2024-02-12 01:03:24 -0800

    重新搞了一下

---
+++ 445Final/Assets/Script/LevelTransition.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelTransition : MonoBehaviour
+{
+    public string nextLevel;
+    // Start is called before the first frame update
+    void Start()
+    {
+    //Debug.log("hello world!");
+   //Debug.log("hi");    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
